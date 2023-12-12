92197b83355d1f5819e318cf2bd8a621bddb73f5
jsdeveloper0401 <wolfmaks85@gmail.com>
2023-12-12 20:47:07 +0500

    added 3-dars js

---
+++ js/3-dars.js
@@ -0,0 +1,185 @@
+// if1
+
+// let n = +prompt("n ni kiriting: ");
+// first
+// if (n > 0) {
+//   n++;
+// } else {
+//   n ;
+// }
+// second
+// n > 0 ? n++ : n;
+
+// console.log(n);
+
+// if2
+// let n = +prompt("n ni kiriting: ");
+// // first
+// if (n > 0) {
+//     n++;
+// } else if (n < 0) {
+//     n -= 2;
+// } else {
+//     n == 0;
+// }
+//  n > 0 ? n++ : (n -= 2);
+
+// if3
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let c = +prompt("c ni kiriting: ");
+
+// let positiveCount = 0;
+// let negativeCount = 0;
+
+// if (a > 0) {
+//   positiveCount++;
+// } else if (a < 0) {
+//   negativeCount++;
+// }
+
+// if (b > 0) {
+//   positiveCount++;
+// } else if (b < 0) {
+//   negativeCount++;
+// }
+
+// if (c > 0) {
+//   positiveCount++;
+// } else if (c < 0) {
+//   negativeCount++;
+// }
+
+// console.log(positiveCount);
+// console.log(negativeCount);
+
+// if4
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// if (a > b) {
+//     console.log(a);
+// } else {
+//     console.log(b);
+// }
+
+// if5
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+
+// if (a > b) {
+//     console.log(b);
+// }
+
+// if (a < b) {
+//     console.log(a);
+// }
+
+// if (a == b) {
+//     console.log(a, b, " bu ikki son o'zaro teng");
+// }
+
+// a > b && console.log("kichik sonning tartib raqami", 2);
+
+// a < b && console.log("kichik sonning tartib raqami", 1);
+
+// if6
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// if (a > b) {
+//     console.log(a, "and", b);
+// } else {
+//     console.log(b, "and", a);
+// }
+// if (a == b) {
+//     console.log(a, "and", b, " bu ikki son o'zaro teng");
+// }
+
+// if7
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let x = a;
+// if (a < b) {
+//     console.log(a, "and", b);
+// } else if (a > b) {
+//     a = b;
+//     b = x;
+//     console.log(a, "and", b);
+// }
+
+// if8
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let res = a + b;
+
+// if (a !== b) {
+//     console.log(res, " ", a, "and", b);
+// } else if (a == b) {
+//     console.log(0," ", a, "and", b);
+// }
+
+// if9
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+
+// if (a > b) {
+//     console.log(a, " ", a, "and", b);
+// } else if (a < b) {
+//     console.log(b, " ", a, "and", b);
+// } else if (a == b) {
+//     console.log(0, " ", a, "and", b);
+// }
+
+// if10
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let c = +prompt("c ni kiriting: ");
+
+// if (a < b && a < c) {
+//     console.log(a);
+// } else if (b < a && b < c) {
+//     console.log(b);
+// } else if (c < a && c < b) {
+//     console.log(c);
+// } else if (a == b && a == c && b == c){
+//   console.log(a, b, c, " bu sonlar o'zaro teng");
+// }
+// if11
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let c = +prompt("c ni kiriting: ");
+
+// if (a < b && a < c && b<c) {
+//     console.log(b);
+// } else if (b < a && b < c && c < a) {
+//     console.log(c);
+// } else if (c < a && c < b && a < b) {
+//     console.log(a);
+// } else if (a == b && a == c && b == c){
+//   console.log(a, b, c, " bu sonlar o'zaro teng");}
+
+// if12
+
+// if13
+
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let c = +prompt("c ni kiriting: ");
+// if(a>b && b<c){
+//   console.log();
+// }
+
+// if16
+// let x = +prompt("x ni kiriting: ");
+// if (x <= 0) {
+//     console.log(-x);
+// } else if (x > 0 && x < 2) {
+//     console.log(Math.pow(x, 2));
+// } else if(x>= 2) {
+//   console.log(4);
+// }
